54afc79224115248796b9c73904de471349c20b1
Leepromax <94459172+Leepromax@users.noreply.github.com>
2021-11-16 22:09:54 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '57367eb11dc94f59bca3ca7004444bf7?v=f24747559ae94971bc1c30c22e9b3492',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 57367eb11dc94f59bca3ca7004444bf7,
+  rootNotionSpaceId:null,
 
   // basic site info (required)
   name: ' Travel',
